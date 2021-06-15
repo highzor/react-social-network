cb02640653ab8cb363a8ecbdebcbcebbd2b9c867
highzor <bnmmnb3@list.ru>
2021-06-16 01:27:10 +0300

    feat: add createRef() method

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,15 +6,22 @@ const MyPosts = (props) => {
 
   const postsElements = props.posts.map(el => <Post message={el.message} likesCount={el.likesCount} />);
 
+  const newPostElement = React.createRef();
+
+  const addPost = () => {
+    const text = newPostElement.current.value;
+    alert(text);
+  };
+
   return (
     <div className={classes.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea />
+          <textarea ref={newPostElement} />
         </div>
         <div>
-          <button>Add</button>
+          <button onClick={addPost}>Add</button>
         </div>
       </div>
       <div className={classes.posts}>
